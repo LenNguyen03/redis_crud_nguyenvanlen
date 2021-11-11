278094e2f582d148d103b4374c45ad2f4a8a7bd7
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-11-11 21:18:22 +0700

    CRUD LIST ok

--- src/main/java/com/example/redis_crud_nguyenvanlen/controller/EmployeeController.java
+++ src/main/java/com/example/redis_crud_nguyenvanlen/controller/EmployeeController.java
@@ -6,38 +6,62 @@ import com.example.redis_crud_nguyenvanlen.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping
 public class EmployeeController {
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @PostMapping("/employees")
-    public Employee saveEmployee(@RequestBody Employee employee){
-        employeeRepository.saveEmployee(employee);
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        employeeRepository.saveEmployeeForHash(employee);
         return employee;
     }
-//
-    @GetMapping("/employees")
-    public Long findAll(){
 
+    @GetMapping("/employees/{id}")
+    public Employee findById(@PathVariable("id") Integer id) {
+        return employeeRepository.findById(id);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
         return employeeRepository.findAll();
     }
-//    @GetMapping("/employees/{id}")
-//    public Employee findById(@PathVariable("id") Integer id){
-//
-//        return employeeRepository.findById(id);
+
+//    @PutMapping("/employee")
+//    public void update(@RequestBody Employee employee) {
+//        employeeRepository.update(employee);
 //    }
-
-    @PutMapping("/employee}")
-    public void update(@RequestBody Employee employee){
-
-        employeeRepository.update(employee);
-    }
 
     @DeleteMapping("/employees/{id}")
     public void delete(@PathVariable("id") Integer id){
         employeeRepository.delete(id);
     }
 
+//////////////////////////////////LIST OPS////////////////////////////////////////////////////
+
+    @PostMapping("/list")
+    public Employee saveEmployeeByList(@RequestBody Employee employee) {
+        employeeRepository.saveEmployeeForList(employee);
+        return employee;
+    }
+
+    @GetMapping("/list/{index}")
+    public Employee findByEmployeeByIndex(@PathVariable("index") long index) {
+        return employeeRepository.getEmployeeAtIndex(index);
+    }
+
+    @DeleteMapping("/list")
+    public void DeleteForList(@RequestBody Employee employee) {
+        employeeRepository.deleteAll(employee);
+        System.out.println("Delete Sucess !");
+    }
+
+    @GetMapping("/list/number")
+    public long getNumberOfEmployees() {
+        return employeeRepository.getNumberOfEmployees();
+    }
 }
--- src/main/java/com/example/redis_crud_nguyenvanlen/repository/EmployeeRepository.java
+++ src/main/java/com/example/redis_crud_nguyenvanlen/repository/EmployeeRepository.java
@@ -4,67 +4,66 @@ import com.example.redis_crud_nguyenvanlen.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class EmployeeRepository {
+    private static final String KEY = "employees";
 
-    private HashOperations hashOperations;//crud hash
-
-
-    private ListOperations listOperations; //crud list
-
-
-
+    private HashOperations hashOperations;
+    private ListOperations<String, Employee> listOperations;
     private RedisTemplate redisTemplate;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
-//        this.hashOperations = redisTemplate.opsForHash();
-        //---------------------------------------------CRUD LIST-------------------------------------------------------
+        this.hashOperations = redisTemplate.opsForHash();
+       ////////CRUD LIST///////////////////////////////
         this.listOperations = redisTemplate.opsForList();
-        this.redisTemplate = redisTemplate;
 
+        this.redisTemplate = redisTemplate;
     }
-    //---------------------------------------------CRUD HASH-------------------------------------------------------
-//    public void saveEmployee(Employee employee){
-//        hashOperations.put("EMPLOYEE", employee.getId(), employee);
-//    }
-//    public List<Employee> findAll(){
-//
-//        return hashOperations.values("EMPLOYEE");
-//    }
-//    public Employee findById(Integer id){
-//
-//        return (Employee) hashOperations.get("EMPLOYEE", id);
-//    }
-//
+
+
+    public void saveEmployeeForHash(Employee employee) {
+        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+    }
+
+    public Employee findById(Integer id){
+
+        return (Employee) hashOperations.get("EMPLOYEE", id);
+    }
+
+
 //    public void update(Employee employee){
 //        saveEmployee(employee);
 //    }
-//    public void delete(Integer id){
-//        hashOperations.delete("EMPLOYEE", id);
-//    }
-
-    //-----------------------------------------------------------CRUD LIST---------------------------------------------------------------------
-    public void saveEmployee(Employee employee){
-        listOperations.leftPush(employee.getId(), employee);
-    }
-
-    public Long findAll(){
-
-        return listOperations.size(Employee.class);
-    }
-//    public Employee findById(Integer id){
-//
-//        return (Employee) listOperations.get("EMPLOYEE", id);
-//    }
-
-    public void update(Employee employee){
-        saveEmployee(employee);
-    }
     public void delete(Integer id){
-        listOperations.remove("EMPLOYEE",1, id);
+        hashOperations.delete("EMPLOYEE", id);
     }
 
-    //-----------------------------------------------------------CRUD SET------------------------------------
+
+    public List<Employee> findAll() {
+        return hashOperations.values(KEY);
+    }
+
+    /////////////////////////////////CRUD LIST///////////////////////////////////////////////
+    public void saveEmployeeForList(Employee employee) {
+        listOperations.leftPush(KEY, employee);
+    }
+
+    public Employee getEmployeeAtIndex(Long index) {
+        return listOperations.index(KEY, index);
+    }
+
+
+    public void deleteAll(Employee employee) {
+        listOperations.remove(KEY, 1, employee);
+    }
+
+    public long getNumberOfEmployees() {
+        return listOperations.size(KEY);
+
+    }
 }
